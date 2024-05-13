da21f63c36b4b5f2488fe25f506531bdbfde9b06
gleb <gleb.gtv@gmail.com>
2024-05-13 15:10:00 +0700

    Бред.

--- combine.sh
+++ combine.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Перенос всего кода в один файл.
-cat init/**/*.py > glebglebgleb.py
-cat db_manager.py >> glebglebgleb.py
-cat gui.py >> glebglebgleb.py
-cat main.py >> glebglebgleb.py
+cat init/**/*.py > AAAAGLEBglebGLEBgleb.py
+cat db_manager.py >> AAAAGLEBglebGLEBgleb.py
+cat gui.py >> AAAAGLEBglebGLEBgleb.py
+cat main.py >> AAAAGLEBglebGLEBgleb.py
